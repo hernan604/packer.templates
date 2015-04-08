1435fc86bb515dbfa3c10a15d0fcad03df2b4c82
Hernan <hernanlopes@gmail.com>
2015-04-08 22:03:11 +0000

    fixing template and shell scripts

--- script/ubuntu/update.sh
+++ script/ubuntu/update.sh
@@ -8,7 +8,14 @@ apt-get -y update
 apt-get -y upgrade
 
 # Install some software
-apt-get -y install build-essential vim-tiny exuberant-ctags git-core redis-server postgresql-server-dev-all postgresql-client postgresql-9.3
+apt-get -y install build-essential 
+apt-get -y install vim 
+apt-get -y install exuberant-ctags 
+apt-get -y install git-core 
+apt-get -y install redis-server 
+apt-get -y install postgresql-server-dev-all 
+apt-get -y install postgresql-client 
+apt-get -y install postgresql-9.3
 
 # Install dependencies
 apt-get -y install dkms
