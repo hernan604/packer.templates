8dc36175380df15ba2efbfd8be2fb3c9ecd186e4
Hernan <hernanlopes@gmail.com>
2015-04-09 12:37:31 +0000

    fixed script

--- script/common/perlbrew.sh
+++ script/common/perlbrew.sh
@@ -2,17 +2,17 @@
 
 COMMAND="
 wget -O - http://install.perlbrew.pl | bash
-source ~/perl5/perlbrew/etc/bashrc      
-echo 'source ~/perl5/perlbrew/etc/bashrc' >> ~/.bashrc  
-perlbrew init                                                                    
-perlbrew install 5.20.1 -j 4                                                     
-perlbrew switch 5.20.1                                                           
-perlbrew install-cpanm                                                           
-cpanm Selenium::Remote::Driver JSON::XS File::Slurp MIME::Base64 Digest::SHA1    
-cpanm Config::Any Moo Mojo::Pg SQL::Abstract DDP POE::Component::Server::IRC     
-cpanm Mojo::Redis2 Mojolicious DateTime                                          
-mkdir ~/perl                                                                     
-cd ~/perl                                                                        
+source ~/perl5/perlbrew/etc/bashrc
+echo 'source ~/perl5/perlbrew/etc/bashrc' >> ~/.bashrc
+perlbrew init
+perlbrew -j 4 install 5.21.9
+perlbrew switch 5.20.1
+perlbrew install-cpanm
+cpanm Selenium::Remote::Driver JSON::XS File::Slurp MIME::Base64 Digest::SHA1
+cpanm Config::Any Moo Mojo::Pg SQL::Abstract DDP POE::Component::Server::IRC
+cpanm Mojo::Redis2 Mojolicious DateTime
+mkdir ~/perl
+cd ~/perl
 git clone https://github.com/hernan604/Web-IRC.git ~/perl/
 "
 sudo su - vagrant -c "$COMMAND"
